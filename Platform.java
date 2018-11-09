9e298f9c4a27348e44a06aa494c9b3f484224192
Lokesh Patel <patellokesh05899@gmail.com>
2018-11-09 11:20:28 +0530

    Added performAction method

--- Platform.java
+++ Platform.java
@@ -1,9 +1,13 @@
-import java.util.Set;
+import java.util.*;
 
 public class Platform {
 	private Set<User> users;
 
 	Platform() {
 		users = new TreeSet<User>();
+	}
+
+	public void performAction(String actionMsg) {
+		//TODO
 	}
 }
